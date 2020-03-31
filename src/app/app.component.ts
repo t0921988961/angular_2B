f716e0766e6b16aac7b9a33fd50db24df3489b30
Luke Cheng <t0921988961@yahoo.com.tw>
2020-03-31 23:21:07 +0800

    設定下拉選單更換英文語系

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,22 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
   title = 'demo1';
+  langs = ['zh-TW', 'en-US'];
 
   constructor(private translateService: TranslateService) {
     this.translateService.setTranslation('zh-TW', {
       Hello: '哈囉'
     });
+    this.translateService.setTranslation('en-US', {
+      Hello: 'Hello Hello'
+    });
   }
 
   ngOnInit(): void {
     this.translateService.use('zh-TW');
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.use(lang);
   }
 }
