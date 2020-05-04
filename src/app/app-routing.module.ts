5978e3ac5a6eddaf7364584fbd4a8c067eb5d6dd
Luke Cheng <t0921988961@yahoo.com.tw>
2020-05-04 17:23:18 +0800

    設定多國語系字串 完成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
+      // preloadingStrategy: PreloadAllModules,
       // enableTracing: true, // use debug route path
     })
   ],
