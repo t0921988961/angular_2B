bd548446641ea9e29c91a65f1cc2d389dacd7e4c
Luke Cheng <t0921988961@yahoo.com.tw>
2020-05-13 16:33:42 +0800

    移動 bannerLists 陣列進 ngOnInit( ){ } 內

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,8 @@ export class HomeComponent implements OnInit {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
+    parallax: true,
+    speed: 600,
     autoplay: {
       delay: 5000,
     },
@@ -28,56 +30,57 @@ export class HomeComponent implements OnInit {
     },
   };
 
-  bannerLists = [
-    //  Major Banner
-    {
-      region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR'],
-      bgImg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_Covid_',
-      bgImgFormat: '.jpg',
-      img: '',
-      video: '',
-      video_bg: '',
-      productName: '',
-      title: '',
-      desc: '',
-      moreBtn_link: 'mailto:info_Pro@@xyzprinting.com?subject=COVID-19 Enquiry&body=Hello',
-      moreBtn_text: '',
-      otherBtn_link: '',
-      otherBtn_text: '',
-    },
-    {
-      region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR', 'th-TH'],
-      bgImg: '',
-      bgImgFormat: '',
-      img: '',
-      video: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/video/2B_Support.mp4',
-      video_bg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/video/2B_Support.png',
-      productName: '',
-      title: '@Html(Messages.get("partpro200xTCS.banner.title"))',
-      desc: '@Messages.get("partpro200xTCS.banner.desc")',
-      moreBtn_link: 'javascripts:;',
-      moreBtn_text: '',
-      otherBtn_link: '',
-      otherBtn_text: '',
-    },
-    {
-      region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR', 'th-TH'],
-      bgImg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_230_bg_',
-      bgImgFormat: '.jpg',
-      img: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_230.png',
-      video: '',
-      video_bg: '',
-      productName: '@Html(Messages.get("index.banner2.productName"))',
-      title: '@Html(Messages.get("index.banner2.title"))',
-      desc: '',
-      moreBtn_link: '/@lang/product/SLS/MfgPro230_xS',
-      moreBtn_text: '',
-      otherBtn_link: '',
-      otherBtn_text: '',
-    },
-  ];
+  bannerLists = null;
 
   ngOnInit() {
+    this.bannerLists = [
+      //  Major Banner
+      {
+        region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR'],
+        bgImg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_Covid_',
+        bgImgFormat: '.jpg',
+        img: '',
+        video: '',
+        video_bg: '',
+        productName: '',
+        title: '',
+        desc: '',
+        moreBtn_link: 'mailto:info_Pro@@xyzprinting.com?subject=COVID-19 Enquiry&body=Hello',
+        moreBtn_text: '',
+        otherBtn_link: '',
+        otherBtn_text: '',
+      },
+      {
+        region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR', 'th-TH'],
+        bgImg: '',
+        bgImgFormat: '',
+        img: '',
+        video: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/video/2B_Support.mp4',
+        video_bg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/video/2B_Support.png',
+        productName: '',
+        title: '"partpro200xTCS.banner.title"|translate',
+        desc: '"partpro200xTCS.banner.desc"|translate',
+        moreBtn_link: '',
+        moreBtn_text: '',
+        otherBtn_link: '',
+        otherBtn_text: '',
+      },
+      {
+        region: ['en-US', 'en-GB', 'fr-FR', 'de-DE', 'ja-JP', 'zh-TW', 'zh-CN', 'ko-KR', 'th-TH'],
+        bgImg: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_230_bg_',
+        bgImgFormat: '.jpg',
+        img: 'https://xyzwebsite.blob.core.windows.net/b2b-landing/renew-b2b-20190902/index/banner/banner_230.png',
+        video: '',
+        video_bg: '',
+        productName: '"index.banner2.productName"|translate',
+        title: '"index.banner2.title"|translate',
+        desc: '',
+        moreBtn_link: '/' + this.translateService.nowLangCode + '/product/SLS/MfgPro230_xS',
+        moreBtn_text: '',
+        otherBtn_link: '',
+        otherBtn_text: '',
+      },
+    ];
   }
 
 }
